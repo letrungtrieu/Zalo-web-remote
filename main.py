2ad20f0bb52025ea2030485de2b54e74dec55124
letrungtrieu <ltrungtrieu@gmail.com>
2023-12-17 20:50:18 +0700

    Revert "update"
    This reverts commit 7818f303b4dd49f6427c780b5250e94dde887ec8.

--- main.py
+++ main.py
@@ -1,198 +1,36 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
-from util import *
 from zalo import Zalo
+import json
+import os,sys
 
-ZALO_THREAD:list[Zalo] = []
+def read_str():
+    fd = sys.stdin.fileno()
+    input = os.read(fd,1024)
+    return input.replace(b'\r\n',b'').decode('utf-8')
 
-def show_actions_window(id):
-    item = tree.item(id)
-    action_window = tk.Toplevel(root)
-    action_window.title("Actions")
+def kill_all_chrome():
+    os.system('taskkill /f /im chrome.exe')
+    os.system('taskkill /f /im python.exe')
+    os.system('cls' if os.name=='nt' else 'clear')
 
-    # Thiết lập kích thước cho cửa sổ phụ
-    action_window_width = 300
-    action_window_height = 350
+if __name__=='__main__':
+    zalos:list[Zalo] = []
 
-    # Lấy kích thước và vị trí của cửa sổ chính
-    root_x = root.winfo_x()
-    root_y = root.winfo_y()
-    root_width = root.winfo_width()
-    root_height = root.winfo_height()
-
-    # Tính vị trí để cửa sổ phụ nằm giữa cửa sổ chính
-    x = root_x + (root_width - action_window_width) // 2
-    y = root_y + (root_height - action_window_height) // 2
-
-    action_window.geometry(f"{action_window_width}x{action_window_height}+{x}+{y}")
-
-    # Frame cho các nút
-    button_frame = tk.Frame(action_window)
-    button_frame.pack(side=tk.TOP, fill=tk.X, pady=10)
-
-    tk.Button(button_frame, text="Start", command=lambda: start_action(action_window,id)).pack(side=tk.LEFT, padx=5)
-    tk.Button(button_frame, text="Stop", command=lambda: stop_action(action_window,item)).pack(side=tk.LEFT, padx=5)
-    tk.Button(button_frame, text="Delete", command=lambda: delete_action(action_window,id)).pack(side=tk.LEFT, padx=5)
-
-    # Frame cho các trường nhập liệu
-    input_frame = tk.Frame(action_window)
-    input_frame.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
-
-    # Group Name
-    tk.Label(input_frame, text="Group Name").pack(side=tk.TOP, fill=tk.X)
-    group_name_entry = tk.Entry(input_frame)
-    group_name_entry.pack(side=tk.TOP, fill=tk.X)
-    group_name_entry.insert(0, item['values'][0])
-
-    # Member Start ID
-    tk.Label(input_frame, text="Member Start ID").pack(side=tk.TOP, fill=tk.X)
-    member_start_id_entry = tk.Entry(input_frame)
-    member_start_id_entry.pack(side=tk.TOP, fill=tk.X)
-    member_start_id_entry.insert(0, item['values'][1])
-
-    # Member End ID
-    tk.Label(input_frame, text="Member End ID").pack(side=tk.TOP, fill=tk.X)
-    member_end_id_entry = tk.Entry(input_frame)
-    member_end_id_entry.pack(side=tk.TOP, fill=tk.X)
-    member_end_id_entry.insert(0, item['values'][2])
-
-    # Message
-    tk.Label(input_frame, text="Message").pack(side=tk.TOP, fill=tk.X)
-    message_entry = tk.Entry(input_frame)
-    message_entry.pack(side=tk.TOP, fill=tk.X)
-    message_entry.insert(0, item['values'][3])
-
-    # Sleep
-    tk.Label(input_frame, text="Sleep").pack(side=tk.TOP, fill=tk.X)
-    sleep_entry = tk.Entry(input_frame)
-    sleep_entry.pack(side=tk.TOP, fill=tk.X)
-    sleep_entry.insert(0, item['values'][4])
+    with open('config.json',  encoding='utf-8') as f:
+        file = f.read()
+        configs:dict = json.loads(file)
+        for dir in list(configs.keys()):
+            config = configs[dir]
+            group_name=config["groupName"]
+            index_start_member=config["memberId"]
+            index_stop_member=config["memberEndId"]
+            message=config["msg"]
+            time_sleep=config["delay"]
+            proxy = config["proxy"]
+            zalos.append(Zalo(dir, proxy, group_name, index_start_member, index_stop_member, message, time_sleep))
     
-    # Profile
-    tk.Label(input_frame, text="Profile").pack(side=tk.TOP, fill=tk.X)
-    profile_entry = tk.Entry(input_frame)
-    profile_entry.pack(side=tk.TOP, fill=tk.X)
-    profile_entry.insert(0, item['values'][5])
-
-    # Nút Lưu (nếu cần)
-    save_button = tk.Button(action_window, text="Save", command=lambda: save_action(action_window, id, group_name_entry.get(), member_start_id_entry.get(), member_end_id_entry.get(), message_entry.get(), sleep_entry.get(), profile_entry.get()))
-    save_button.pack(side=tk.BOTTOM, pady=10)
-
-def get_zalo(item)->Zalo:
-    for zalo in ZALO_THREAD:
-        if zalo.profile == item['values'][5]:
-            return zalo
-
-def start_action(action_window:tk.Toplevel, item_id):
-    # Implement start action
-    item = tree.item(item_id)
-    zalo:Zalo = get_zalo(item)
-    if zalo is not None:
-        ZALO_THREAD.remove(zalo)
-        zalo.stop()
-    zalo = Zalo(tree, item_id,*item['values'])
-    ZALO_THREAD.append(zalo)
-    zalo.start()
-    if action_window:
-        action_window.destroy()
-    print(f"Start Action for {item['values']}")
-
-def stop_action(action_window:tk.Toplevel, item):
-    # Implement stop action
-    zalo:Zalo = get_zalo(item)
-    if zalo is not None:
-        zalo.stop()
-        ZALO_THREAD.remove(zalo)
-    action_window.destroy()
-    print(f"Stop Action for {item['values']}")
-
-def delete_action(action_window:tk.Toplevel, id):
-    # Implement delete action
-    item = tree.item(id)
-    tree.delete(id)
-    save_config(tree)
-    stop_action(action_window, item)
-    action_window.destroy()
-    print(f"Delete Action for {item['values']}")
-
-def save_action(action_window:tk.Toplevel, id, group_name, member_start_id, member_end_id, message, sleep, profile):
-    item = tree.item(id)
-    if item['values'][5] != profile and get_zalo(item):
-        messagebox.showerror("Profile conflict","Profile đã được sử dụng")
-        action_window.destroy()
-        return
-    tree.item(id, values=(group_name, member_start_id, member_end_id, message, sleep, profile))
-    action_window.destroy()
-    save_config(tree)
-
-
-def add_item():
-    id = tree.insert('', tk.END, values=('', 0, 0, '', 120, ''))
-    show_actions_window(id)
-
-def start_all():
-    for item_id in tree.get_children():
-        start_action(None,item_id)
-
-def stop_all():
-    clone = ZALO_THREAD.copy()
-    ZALO_THREAD.clear()
-    for v in clone:
-        try:
-            v.stop()
-        except:
-            pass
-        
-def create_ui():
-    global root, tree
-    root = tk.Tk()
-    root.title("Zalo Automation")
-    root.geometry("640x480")
-
-    style = ttk.Style(root)
-    style.theme_use("default")
-
-    style.configure("Treeview.Heading", font=('Calibri', 10, 'bold'), background="#D3D3D3", foreground='black')
-    style.configure("Treeview", font=('Calibri', 10), background="#D3D3D3", foreground='black')
-
-    tree = ttk.Treeview(root, columns=('GroupName', 'MemberStartID', 'MemberEndID', 'Message', 'Sleep', 'Profile'), show='headings')
-    tree.heading('GroupName', text='Group Name')
-    tree.heading('MemberStartID', text='Start ID')
-    tree.heading('MemberEndID', text='End ID')
-    tree.heading('Message', text='Message')
-    tree.heading('Sleep', text='Sleep')
-    tree.heading('Profile', text='Profile')
     
-
-    tree.column('GroupName', width=120, stretch=tk.YES, anchor='center')
-    tree.column('MemberStartID', width=90, stretch=tk.YES, anchor='center')
-    tree.column('MemberEndID', width=90, stretch=tk.YES, anchor='center')
-    tree.column('Message', width=150, stretch=tk.YES, anchor='center')
-    tree.column('Sleep', width=80, stretch=tk.YES, anchor='center')
-    tree.column('Profile', width=120, stretch=tk.YES, anchor='center')
-
-    config = read_config()
-    for profile, details in config.items():
-        item_id = tree.insert('', tk.END, values=(details['group_name'], details['member_start_id'], details['member_end_id'], details['msg'], details['sleep'], details['profile']))
-        ZALO_THREAD.append(Zalo(tree, item_id, **details))
-
-
-    tree.bind('<<TreeviewSelect>>', lambda event: show_actions_window(tree.selection()[0]))
-    tree.pack(expand=True, fill='both')
-
-    button_frame = tk.Frame(root)
-    button_frame.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
+    for zalo in zalos:
+        t = zalo.start()
     
-    add_button = tk.Button(button_frame, text="ADD", command=add_item)  # Cần thực hiện chức năng thêm mới
-    add_button.pack(side=tk.TOP, fill=tk.X)
-    
-    start_all_button = tk.Button(button_frame, text="START ALL", command=start_all)  # Cần thực hiện chức năng thêm mới
-    start_all_button.pack(side=tk.TOP, fill=tk.X)
-    
-    stop_all_button = tk.Button(button_frame, text="STOP ALL", command=stop_all)  # Cần thực hiện chức năng thêm mới
-    stop_all_button.pack(side=tk.TOP, fill=tk.X)
-
-    root.mainloop()
-
-if __name__ == '__main__':
-    create_ui()
+    for t in zalos:
+        t.join()
--- zalo.py
+++ zalo.py
@@ -6,54 +6,36 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait as W
 from selenium.webdriver.support import expected_conditions as E
 from threading import Thread
-import os,time,re
-from util import *
-import logging
+import os,time,re,logging
 
 _logger = logging.getLogger(__name__)
 
 
 class Zalo(Thread):
     
-    def __init__(self, tree, item_id, group_name: str, member_start_id: int, member_end_id: int, msg: str, sleep: float, profile):
+    def __init__(self, user_path, proxy:str, group_name: str, member_index_id: int, member_index_stop_id: int, msg: str, sleep: float):
         Thread.__init__(self)
-        self.tree = tree
-        self.item_id = item_id
-        self.profile = profile
-        self.chrome = None
-        self.group_name = re.sub(r"[\n\r\t]", "", group_name)
-        self.member_index_id = int(member_start_id)
-        self.member_index_stop_id = int(member_end_id)
-        self.msg = msg
-        self.sleep = float(sleep)
-        self.is_stop = False
-
-    def update(self, group_name: str, member_start_id: int, member_end_id: int, msg: str, sleep: float, profile):
-        self.profile = profile
-        self.chrome = None
-        self.group_name = re.sub(r"[\n\r\t]", "", group_name)
-        self.member_index_id = int(member_start_id)
-        self.member_index_stop_id = int(member_end_id)
-        self.msg = msg
-        self.sleep = float(sleep)
-        self.is_stop = False
-    
-    def tree_update(self):
-        values = self.tree.item(self.item_id)['values']
-        values[1] = self.member_index_id + 1
-        self.tree.item(self.item_id, values=values)
-        save_config(self.tree)
-    
-    def get_options(self):
-        options = Options()
-        options.add_experimental_option(
+        self.profile = user_path
+        self.options = Options()
+        self.options.add_experimental_option(
             'excludeSwitches',
             ["enable-logging"]
         )
-        options.add_argument(f'user-data-dir={os.getcwd()}\\profiles\\{self.profile}')
-        options.binary_location = f'bin/chrome.exe'
-        return options
-    
+        self.options.binary_location = f'bin/chrome.exe'
+        if proxy:
+            self.options.add_argument(f'--proxy-server=http://{proxy}')
+        self.options.add_argument(f'user-data-dir={os.getcwd()}\\profiles\\{user_path}')
+        chrome = webdriver.Chrome(service=Service(executable_path=f"chrome/chromedriver.exe"), options=self.options)
+        chrome.get("https://zalo.me/zalo-chat")
+        self.chrome = chrome
+        self.wait_element = W(self.chrome, 120)
+        self.group_name = group_name
+        self.member_index_id = member_index_id
+        self.member_index_stop_id = member_index_stop_id
+        self.msg = msg
+        self.sleep = sleep
+        self.is_stop = False
+
     def send_msg_for_member_of_group(self):
         print(f"------{self.profile}-------")
         search_input: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
@@ -80,82 +62,72 @@ class Zalo(Thread):
             "div.chat-box-member__info__name.v2"
         )))
 
+        while self.member_index_id < self.member_index_stop_id:
+            name_el: WebElement = member_list[self.member_index_id].find_element(by=By.CSS_SELECTOR, value="div.truncate")
+            name_member:str = name_el.text
+            if not name_member:
+                continue
+            flag = re.search("Tài khoản bị khóa", name_member) or re.search("Account(.*)Banned", name_member)
+            if flag:
+                print(f"------STT   {self.member_index_id}-------")
+                print("------Tài khoản bị khóa-----")
+                print("----------------------------------------------------------------")
+                self.member_index_id +=1
+            else:
+                print(">>>>>>Gửi tin nhắn cho ", name_member)
+                member_list[self.member_index_id].click()
+                btn_msg: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
+                    By.CSS_SELECTOR,
+                    'div[data-translate-inner="STR_CHAT"]'
+                )))
+                btn_msg[0].click()
+                time.sleep(2)
+                send_msg: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
+                    By.CSS_SELECTOR,
+                    "div#input_line_0"
+                )))
+                send_msg[0].send_keys(self.msg)
+                time.sleep(1)
+                qick_message: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
+                                    By.CSS_SELECTOR,
+                                    'div[class="qri clickable active"]'
+                                )))
+                time.sleep(3)
+                qick_message[0].click()
+                time.sleep(3)
+                btn_send_msg: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
+                    By.CSS_SELECTOR,
+                    'div[data-translate-inner="STR_SEND"]'
+                )))
+                btn_send_msg[0].click()
+                print(f"------STT   {self.member_index_id}-------")
+                print("------Đã gửi thành công-----")
+                print("----------------------------------------------------------------")
+                time.sleep(self.sleep)
+                break
         
-        name_el: WebElement = member_list[self.member_index_id].find_element(by=By.CSS_SELECTOR, value="div.truncate")
-        name_member:str = name_el.text
-        if not name_member:
-            return
-        flag = re.search("Tài khoản bị khóa", name_member) or re.search("Account(.*)Banned", name_member)
-        if flag:
-            print(f"------STT   {self.member_index_id}-------")
-            print("------Tài khoản bị khóa-----")
-            print("----------------------------------------------------------------")
-            self.member_index_id +=1
-        else:
-            print(">>>>>>Gửi tin nhắn cho ", name_member)
-            member_list[self.member_index_id].click()
-            btn_msg: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
-                By.CSS_SELECTOR,
-                'div[data-translate-inner="STR_CHAT"]'
-            )))
-            btn_msg[0].click()
-            time.sleep(2)
-            send_msg: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
-                By.CSS_SELECTOR,
-                "div#input_line_0"
-            )))
-            send_msg[0].send_keys(self.msg)
-            time.sleep(1)
-            qick_message: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
-                                By.CSS_SELECTOR,
-                                'div[class="qri clickable active"]'
-                            )))
-            time.sleep(0.5)
-            qick_message[0].click()
-            time.sleep(3)
-            btn_send_msg: list[WebElement] = self.wait_element.until(E.presence_of_all_elements_located((
-                By.CSS_SELECTOR,
-                'div[data-translate-inner="STR_SEND"]'
-            )))
-            btn_send_msg[0].click()
-            print(f"------STT   {self.member_index_id}-------")
-            print("------Đã gửi thành công-----")
-            print("----------------------------------------------------------------")
-            self.tree_update()
-            time.sleep(self.sleep)
-            
-                
+        
+        
 
     def stop(self):
         self.is_stop = True
-        if self.chrome:
-            self.chrome.close()
+        self.chrome.close()
         del self
 
     def run(self):
-        chrome = webdriver.Chrome(service=Service(executable_path=f"chrome/chromedriver.exe"), options=self.get_options())
-        chrome.get("https://zalo.me/zalo-chat")
-        self.chrome = chrome
-        self.wait_element = W(self.chrome, 120)
-        
         while True:
-            try:
-                self.send_msg_for_member_of_group()
-                self.member_index_id += 1
-                if self.is_stop or self.member_index_id > self.member_index_stop_id:
-                    print(f"Group {self.group_name} OK rồi nha ")
-                    break
-            except Exception as e:
-                _logger.error(e)
-                self.stop()
+            # try:
+            self.send_msg_for_member_of_group()
+            self.member_index_id += 1
+            if self.is_stop or self.member_index_id > self.member_index_stop_id:
+                print(f"Group {self.group_name} OK rồi nha ")
                 break
-                # print("Có lỗi xảy ra đang khởi động lại Chrome")
-                # time.sleep(1)
-                # if self.is_stop or self.member_index_id > self.member_index_stop_id:
-                #     break
-                # if self.chrome:
-                #     self.chrome.quit()
-                #     del self.chrome
-                # self.chrome = webdriver.Chrome(service=Service(executable_path=f"chrome/chromedriver.exe"), options=self.get_options())
-                # self.chrome.get("https://zalo.me/zalo-chat")
-                # self.wait_element = W(self.chrome, 120)
+            # except Exception as e:
+            #     print(e)
+            #     print("Có lỗi xảy ra đang khởi động lại Chrome")
+            #     time.sleep(10)
+            #     self.chrome.quit()
+            #     del self.chrome
+            #     self.chrome = webdriver.Chrome(f"{os.getcwd()}\\chrome\\chromedriver.exe", options=self.options)
+            #     self.chrome.get("https://zalo.me/zalo-chat")
+            #     self.wait_element = W(self.chrome, 120)
